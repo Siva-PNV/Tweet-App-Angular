841fb32886acd715626e83542b8ffc40df3b5c89
Siva Pallem <74588031+Siva-PNV@users.noreply.github.com>
2022-06-27 13:13:45 +0530

    updated path
    updated path

--- src/app/services/login/login-service.service.ts
+++ src/app/services/login/login-service.service.ts
@@ -19,7 +19,7 @@ export class LoginService {
   loggedIn: boolean;
   // baseUrl = "http://localhost:8080/api/v1.0/tweets";
   baseUrl =
-    "http://tweetapp-env.eba-ih3pr6wj.us-east-1.elasticbeanstalk.com/api/v1.0/tweets";
+    "http://tweetapp-env.eba-gymvfjnz.us-east-1.elasticbeanstalk.com/api/v1.0/tweets";
   constructor(private http: HttpClient) {}
 
   public checkUserCredentials(
